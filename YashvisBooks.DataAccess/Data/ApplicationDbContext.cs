b99b90946833e4c6a6ae6e18f9467a246e5db7da
Yashvi Brijmohan Raja <W0766432@myscc.ca>
2021-11-08 12:33:20 -0500

    Add new class , table and migration for Category

--- YashvisBooks.DataAccess/Data/ApplicationDbContext.cs
+++ YashvisBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using YashvisBooks.Models;
 
 namespace YashvisBookStore.DataAccess.Data
 {
@@ -12,5 +13,7 @@ namespace YashvisBookStore.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ YashvisBooks.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace YashvisBooks.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="Category Name")] 
+        [Required]
+        [MaxLength(50)]
+
+        public string Name { get; set; }
+    }
+}
