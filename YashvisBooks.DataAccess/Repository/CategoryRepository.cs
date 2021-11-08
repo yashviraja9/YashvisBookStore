97218592c87985b372ea6ef54075f62b5802203f
Yashvi Brijmohan Raja <W0766432@myscc.ca>
2021-11-08 13:57:25 -0500

    add category repository

---
+++ YashvisBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using YashvisBooks.DataAccess.Repository.IRepository;
+using YashvisBooks.Models;
+using YashvisBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YashvisBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Upadte(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
