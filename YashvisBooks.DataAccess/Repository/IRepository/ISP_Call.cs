431cd666eb161a85cd9eada5dd1280af21de4d3d
yashviraja9 <90658729+yashviraja9@users.noreply.github.com>
2021-11-11 15:44:44 -0500

    add comments to ISP_Call.cs

--- YashvisBooks.DataAccess/Repository/IRepository/ISP_Call.cs
+++ YashvisBooks.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -9,13 +9,23 @@ namespace YashvisBooks.DataAccess.Repository.IRepository
 {
     public interface ISP_Call : IDisposable
     {
+        // eg. first column of first row in the result set
+        
         T Single<T>(string procedurename, DynamicParameters param = null);
+        
+        // execute something to the database but not retrieve anything
 
         void Execute(string procedurename, DynamicParameters param = null);
+        
+        //retrieves the complete row or record
 
         T OneRecord<T>(string procedurename, DynamicParameters param = null);
+        
+        //get all of the rows
 
         IEnumerable<T> List<T>(string procedurename, DynamicParameters param = null);
+        
+        // stored procedure that returns two tables
 
         Tuple<IEnumerable<T1>,IEnumerable<T2>> List<T1, T2>(string procedurename, DynamicParameters param = null);
     }
