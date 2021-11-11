1f9b384d3a43eb6bb162d5f72008719e31955e19
yashviraja9 <90658729+yashviraja9@users.noreply.github.com>
2021-11-11 16:02:18 -0500

    Add comment to IRepository.cs

--- YashvisBooks.DataAccess/Repository/IRepository/IRepository.cs
+++ YashvisBooks.DataAccess/Repository/IRepository/IRepository.cs
@@ -9,12 +9,13 @@ namespace YashvisBooks.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        T Get(int id);
+        T Get(int id);                  // Retrieve a category from the database by id 
+        //List of Categories based on requirements 
 
         IEnumerable<T> GetAll(
             Expression<Func<T, bool>> filter = null,
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            string includeProperties = null
+            string includeProperties = null             // useful  for foreign key references
             );
 
         T GetFirstOrDefault(
